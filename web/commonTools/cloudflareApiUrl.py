07a33f45ea7fa2bb3b8d137b380835a5c5b33137
tingyanqinxing <tingyanqinxing@126.com>
2019-09-07 13:20:58 +0800

    d
    ddd

--- web/commonTools/cloudflareApiUrl.py
+++
@@ -1,8 +0,0 @@
-authEmail = "helaowang@gmail.com"
-authKey = "799bdf658832da5b74be6c0a4a2b35d3a43e3"
-
-getZoneIDUrl = "https://api.cloudflare.com/client/v4/zones"
-getUserIDUrl = "https://api.cloudflare.com/client/v4/user"
-getAccountIDUrl = "https://api.cloudflare.com/client/v4/accounts"
-
-
