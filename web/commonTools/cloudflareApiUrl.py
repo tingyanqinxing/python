9fb4877d9751b0c37ab4e81df3f5c9f3f9340a7b
tingyanqinxing <tingyanqinxing@126.com>
2019-09-07 13:14:56 +0800

    Update cloudflareApiUrl.py

--- web/commonTools/cloudflareApiUrl.py
+++ web/commonTools/cloudflareApiUrl.py
@@ -1,5 +1,5 @@
-authEmail = "helaowang@gmail.com"
-authKey = "799bdf658832da5b74be6c0a4a2b35d3a43e3"
+authEmail = "*@gmail.com"
+authKey = "*"
 
 getZoneIDUrl = "https://api.cloudflare.com/client/v4/zones"
 getUserIDUrl = "https://api.cloudflare.com/client/v4/user"
