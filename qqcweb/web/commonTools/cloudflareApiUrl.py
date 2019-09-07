026b24592adea02a7bf8eabd5132e3b12ec91b0d
flint <qqc.flint@gmail.com>
2019-09-07 13:19:07 +0800

    d

--- qqcweb/web/commonTools/cloudflareApiUrl.py
+++
@@ -1,4 +0,0 @@
-
-getZoneIDUrl = "https://api.cloudflare.com/client/v4/zones"
-getUserIDUrl = "https://api.cloudflare.com/client/v4/user"
-getAccountIDUrl = "https://api.cloudflare.com/client/v4/accounts"
